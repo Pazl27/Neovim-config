65301483403be66fa440b69974f61647b7979520
Pazl <jaul.ponas@gmail.com>
2024-07-31 22:52:58 +0200

    markview added

--- lua/paul/plugins/markdown.lua
+++ lua/paul/plugins/markdown.lua
@@ -1,14 +1,12 @@
 return {
-  'MeanderingProgrammer/markdown.nvim',
-    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter', -- Mandatory
-        'nvim-tree/nvim-web-devicons', -- Optional but recommended
-    },
-    config = function()
-        require('render-markdown').setup({
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
 
-      bullets = { '-', '○', '◆', '◇' },
-    })
-    end,
+    dependencies = {
+        -- You may not need this if you don't lazy load
+        -- Or if the parsers are in your $RUNTIMEPATH
+        "nvim-treesitter/nvim-treesitter",
+
+        "nvim-tree/nvim-web-devicons"
+    },
 }
